0644a73d8b1b900baba544dbdf5e4e347a50360e
Abhijeet Sharma <99397886+AbhijeetSharma1@users.noreply.github.com>
2022-07-01 19:18:26 +0530

    Add files via upload

---
+++ My-Leetcode-Solutions/twosum.js
@@ -0,0 +1,18 @@
+
+// © 2022 ABHIJEET SHARMA
+
+for(let abhi =0 ; abhi<nums.length ; abhi++){
+    for(let jeet = abhi+1 ; jeet<nums.length ; jeet++){
+        if(nums[abhi] + nums[jeet] === target){
+            return [abhi, jeet]
+        }
+    }
+}
+
+// NOTE : ☝ Note that this code is meant to be used for learning and researching purposes only,
+//  and it is not meant to be used for ❌❌❌ PLAGIARISM ❌❌❌
+
+
+// © 2022 ABHIJEET SHARMA
+
+
